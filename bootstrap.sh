462676ca1285b9111c345baea7471db0742669a4
Dan Brown <roooms@gmail.com>
2017-09-08 11:33:51 +0100

    More verbose bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,15 @@
 set -e
 #set -x
 
+echo "--> Update apt"
 sudo apt-get update --fix-missing
+echo "--> Install base packages"
 sudo apt-get install --yes zip unzip git make dnsmasq
 
-# configure dnsmasq
-cat > 10-consul <<'EOF'
+echo "--> Configure dnsmasq"
+cat > /tmp/10-consul <<'EOF'
 server=/consul/127.0.0.1#8600
 EOF
 sudo mkdir -p /etc/dnsmasq.d
-sudo mv 10-consul /etc/dnsmasq.d
+sudo mv /tmp/10-consul /etc/dnsmasq.d
 sudo service dnsmasq restart
