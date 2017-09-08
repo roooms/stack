14e79438a7285f030eda0a76b2558a182086594d
Dan Brown <roooms@gmail.com>
2017-09-08 13:25:58 +0100

    Use external IP for vault and enable UI

--- config/etc_vault.d_default.hcl
+++ config/etc_vault.d_default.hcl
@@ -6,3 +6,4 @@ listener "tcp" {
   address = "{{ vault_ip }}:8200"
   tls_disable = "true"
 }
+ui = true
--- configure-vault-server.sh
+++ configure-vault-server.sh
@@ -3,7 +3,7 @@ set -e
 #set -x
 
 SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})"
-VAULT_IP="127.0.0.1"
+VAULT_IP="$(ip route | grep "enp0s8" | awk '{print $9}')"
 VAULT_ADDR="http://${VAULT_IP}:8200"
 
 echo "--> Configuring vault server"
