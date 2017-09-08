79dad65ec5cd9e68077beb271c95c2d7351184be
Dan Brown <roooms@gmail.com>
2017-09-08 18:59:28 +0100

    Support bootstrap_expect in top level script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ set -e
 echo "--> Updating apt"
 sudo apt-get update --fix-missing
 echo "--> Installing base packages"
-sudo apt-get install --yes zip unzip git make dnsmasq
+sudo apt-get install --yes zip unzip dnsmasq
 
 echo "--> Configuring dnsmasq"
 cat > /tmp/10-consul <<'EOF'
@@ -13,4 +13,4 @@ server=/consul/127.0.0.1#8600
 EOF
 sudo mkdir -p /etc/dnsmasq.d
 sudo mv /tmp/10-consul /etc/dnsmasq.d
-sudo service dnsmasq restart
+sudo systemctl restart dnsmasq
--- create-hashistack-cluster.sh
+++ create-hashistack-cluster.sh
@@ -4,21 +4,17 @@ set -e
 
 SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})"
 NODE_NAME="$(hostname)"
+BOOTSTRAP_EXPECT="${1:-"3"}"
 
-if [[ ! -f $(which make) ]]; then
-  echo "--> Unable to find make"
-  echo "--> Running bootstrap"
-  bash ${SCRIPT_PATH}/bootstrap.sh
-fi
-
+bash ${SCRIPT_PATH}/bootstrap.sh
 bash ${SCRIPT_PATH}/install-app.sh consul 0.9.2 consul-enterprise_0.9.2+ent_linux_amd64.zip
 bash ${SCRIPT_PATH}/install-app.sh nomad 0.6.2
 bash ${SCRIPT_PATH}/install-app.sh vault 0.8.2 vault-enterprise_0.8.2_linux_amd64.zip
 
 case ${NODE_NAME} in
   node1 | node2 | node3 )
-    bash ${SCRIPT_PATH}/configure-consul-server.sh
-    bash ${SCRIPT_PATH}/configure-nomad-server.sh
+    bash ${SCRIPT_PATH}/configure-consul-server.sh ${BOOTSTRAP_EXPECT}
+    bash ${SCRIPT_PATH}/configure-nomad-server.sh ${BOOTSTRAP_EXPECT}
     bash ${SCRIPT_PATH}/configure-vault-server.sh
     bash ${SCRIPT_PATH}/start-app.sh consul
     bash ${SCRIPT_PATH}/start-app.sh nomad
