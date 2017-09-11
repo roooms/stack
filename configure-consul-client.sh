d06525e3aa3dd93a70bde2bb408c37a70ea4f3d4
Dan Brown <roooms@gmail.com>
2017-09-11 14:42:18 +0100

    Support more config customisation

--- configure-consul-client.sh
+++ configure-consul-client.sh
@@ -4,6 +4,8 @@ set -e
 
 SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})"
 ADVERTISE_ADDR="$(ip route | grep "enp0s8" | awk '{print $9}')"
+DATACENTER="${3:-"vagrant"}"
+RETRY_JOIN="10.0.0.${2:-"11"}"
 NODE_NAME="$(hostname)"
 
 echo "--> Deleting any existing configuration and local data"
@@ -12,6 +14,8 @@ sudo rm -rf /opt/consul/*
 
 echo "--> Configuring consul client"
 sed -e "s/{{ advertise_addr }}/${ADVERTISE_ADDR}/g" \
+    -e "s/{{ datacenter }}/${DATACENTER}/g" \
     -e "s/{{ node_name }}/${NODE_NAME}/g" \
+    -e "s/{{ retry_join }}/${RETRY_JOIN}/g" \
     ${SCRIPT_PATH}/config/etc_consul.d_default.json \
     | sudo tee /etc/consul.d/default.json
--- configure-consul-server.sh
+++ configure-consul-server.sh
@@ -3,8 +3,10 @@ set -e
 #set -x
 
 SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})"
-BOOTSTRAP_EXPECT="${1:-"3"}"
 ADVERTISE_ADDR="$(ip route | grep "enp0s8" | awk '{print $9}')"
+BOOTSTRAP_EXPECT="${1:-"3"}"
+DATACENTER="${3:-"vagrant"}"
+RETRY_JOIN="10.0.0.${2:-"11"}"
 NODE_NAME="$(hostname)"
 
 echo "--> Deleting any existing configuration and local data"
@@ -13,7 +15,9 @@ sudo rm -rf /opt/consul/*
 
 echo "--> Configuring consul server"
 sed -e "s/{{ advertise_addr }}/${ADVERTISE_ADDR}/g" \
+    -e "s/{{ datacenter }}/${DATACENTER}/g" \
     -e "s/{{ node_name }}/${NODE_NAME}/g" \
+    -e "s/{{ retry_join }}/${RETRY_JOIN}/g" \
     ${SCRIPT_PATH}/config/etc_consul.d_default.json \
     | sudo tee /etc/consul.d/default.json
 sed -e "s/0/${BOOTSTRAP_EXPECT}/g" \
