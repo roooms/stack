83c97cc793535964a328f5594c3514e51ca97306
Dan Brown <roooms@gmail.com>
2017-09-11 20:09:32 +0100

    Support additional arguments for two clusters

--- create-hashistack-cluster.sh
+++ create-hashistack-cluster.sh
@@ -3,8 +3,11 @@ set -e
 #set -x
 
 SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})"
+
+BOOTSTRAP_EXPECT="3"
+DATACENTER="primary"
 NODE_NAME="$(hostname)"
-BOOTSTRAP_EXPECT="${1:-"3"}"
+RETRY_JOIN="11"
 
 bash ${SCRIPT_PATH}/bootstrap.sh
 bash ${SCRIPT_PATH}/install-app.sh consul 0.9.2 consul-enterprise_0.9.2+ent_linux_amd64.zip
@@ -13,32 +16,32 @@ bash ${SCRIPT_PATH}/install-app.sh vault 0.8.2 vault-enterprise_0.8.2_linux_amd6
 
 case ${NODE_NAME} in
   node1 | node2 | node3 )
-    bash ${SCRIPT_PATH}/configure-consul-server.sh ${BOOTSTRAP_EXPECT}
-    bash ${SCRIPT_PATH}/configure-nomad-server.sh ${BOOTSTRAP_EXPECT}
+    bash ${SCRIPT_PATH}/configure-consul-server.sh ${BOOTSTRAP_EXPECT} ${RETRY_JOIN} ${DATACENTER}
+    bash ${SCRIPT_PATH}/configure-nomad-server.sh ${BOOTSTRAP_EXPECT} ${DATACENTER}
     bash ${SCRIPT_PATH}/configure-vault-server.sh
     bash ${SCRIPT_PATH}/start-app.sh consul
     bash ${SCRIPT_PATH}/start-app.sh nomad
     bash ${SCRIPT_PATH}/start-app.sh vault
   ;;
   * )
-    bash ${SCRIPT_PATH}/configure-consul-client.sh
-    bash ${SCRIPT_PATH}/configure-nomad-client.sh
+    bash ${SCRIPT_PATH}/configure-consul-client.sh ${BOOTSTRAP_EXPECT} ${RETRY_JOIN} ${DATACENTER}
+    bash ${SCRIPT_PATH}/configure-nomad-client.sh ${BOOTSTRAP_EXPECT} ${DATACENTER}
     bash ${SCRIPT_PATH}/start-app.sh consul
     bash ${SCRIPT_PATH}/start-app.sh nomad
   ;;
 esac
 
-# wait for consul, nomad and vault to sort themselves out
-echo "--> Sleep 10 while consul, nomad and vault get ready"
+echo "--> Sleep 10 while consul gets ready"
 sleep 10
+sudo consul join 10.0.0.11
 
 case ${NODE_NAME} in
   node1 )
     bash ${SCRIPT_PATH}/unseal-vault.sh
   ;;
   node2 | node3 )
-    echo "--> Sleep 10 as node1 might need to vault init"
-    sleep 10 # let node1 init if required
+    echo "--> Sleep 10 as another node might need to vault init"
+    sleep 10
     bash ${SCRIPT_PATH}/unseal-vault.sh
   ;;
   * )
