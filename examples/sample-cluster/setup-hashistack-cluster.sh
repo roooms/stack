9555277ef816bdb7f59c0947324e500b851f8257
Dan Brown <roooms@gmail.com>
2017-09-11 21:42:02 +0100

    Create examples dir structure

--- examples/sample-cluster/setup-hashistack-cluster.sh
+++ examples/sample-cluster/setup-hashistack-cluster.sh
@@ -2,7 +2,7 @@
 set -e
 #set -x
 
-SCRIPT_PATH="$(dirname ${BASH_SOURCE[0]})"
+SCRIPT_PATH="../../$(dirname ${BASH_SOURCE[0]})"
 
 BOOTSTRAP_EXPECT="3"
 DATACENTER="primary"
@@ -28,23 +28,5 @@ case ${NODE_NAME} in
     bash ${SCRIPT_PATH}/configure-nomad-client.sh ${BOOTSTRAP_EXPECT} ${DATACENTER}
     bash ${SCRIPT_PATH}/start-app.sh consul
     bash ${SCRIPT_PATH}/start-app.sh nomad
-  ;;
-esac
-
-echo "--> Sleep 10 while consul gets ready"
-sleep 10
-sudo consul join 10.0.0.11
-
-case ${NODE_NAME} in
-  node1 )
-    bash ${SCRIPT_PATH}/unseal-vault.sh
-  ;;
-  node2 | node3 )
-    echo "--> Sleep 10 as another node might need to vault init"
-    sleep 10
-    bash ${SCRIPT_PATH}/unseal-vault.sh
-  ;;
-  * )
-    exit 0
   ;;
 esac
