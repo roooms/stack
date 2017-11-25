79a359d0987fb79b11b2f9ae8eca6407e681a580
Dan Brown <roooms@gmail.com>
2017-11-25 18:48:19 +0000

    Use Nomad 0.7.0

--- examples/nomad/setup-nomad-acl.sh
+++ examples/nomad/setup-nomad-acl.sh
@@ -8,7 +8,7 @@ RETRY_JOIN="11"
 
 /vagrant/bin/configure-dnsmasq.sh
 /vagrant/bin/install-app.sh consul 0.9.3
-/vagrant/bin/install-app.sh nomad 0.7.0-beta1
+/vagrant/bin/install-app.sh nomad 0.7.0
 
 case ${NODE_NAME} in
   node1 | node2 | node3 )
