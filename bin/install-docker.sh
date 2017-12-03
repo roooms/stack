4257494fd27bff259381adf9dd2dec6ad62172a2
Dan Brown <roooms@gmail.com>
2017-12-02 20:13:48 -0800

    Improve docker install

---
+++ bin/install-docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+#set -x
+
+echo "--> Adding docker keyserver"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>/dev/null
+
+echo "--> Adding docker repo"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+echo "--> Updating cache"
+sudo apt-get update --fix-missing
+
+echo "--> Installing docker"
+sudo apt-get install docker-ce --yes
+
+echo "--> Allowing docker without sudo"
+sudo usermod -aG docker "$(whoami)"
