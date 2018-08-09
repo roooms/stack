5b60b404b5605964e92ad10b9236c9bad80b2e2a
Dan Brown <roooms@gmail.com>
2018-08-09 13:03:05 +0100

    Support using consul or file for vault storage

--- bin/configure-vault-server.sh
+++ bin/configure-vault-server.sh
@@ -4,11 +4,13 @@ set -e
 
 VAULT_IP="$(hostname -I | awk '{print $2}')"
 VAULT_ADDR="http://${VAULT_IP}:8200"
+VAULT_STORAGE="${1:-"consul"}"
 
 echo "--> Configuring vault server"
 # vault configuration file
 sed -e "s/{{ vault_ip }}/${VAULT_IP}/g" \
     /vagrant/etc/vault.d/default.hcl | sudo tee /etc/vault.d/default.hcl
+cat /vagrant/etc/vault.d/storage-${VAULT_STORAGE}.hcl | sudo tee /etc/vault.d/storage.hcl
 # env configuration
 echo "export VAULT_ADDR=${VAULT_ADDR}" | sudo tee /etc/profile.d/vault.sh
 sudo chmod 644 /etc/profile.d/vault.sh
--- etc/vault.d/default.hcl
+++ etc/vault.d/default.hcl
@@ -1,7 +1,3 @@
-storage "consul" {
-  address = "127.0.0.1:8500"
-  path = "vault"
-}
 listener "tcp" {
   address = "{{ vault_ip }}:8200"
   tls_disable = "true"
