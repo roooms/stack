ae06dceb46e2640a15440620728a38a09fadd312
Dan Brown <roooms@gmail.com>
2018-08-09 13:07:11 +0100

    Vault unseal to files rather than requiring consul

--- bin/vault-unseal.sh
+++ bin/vault-unseal.sh
@@ -2,30 +2,20 @@
 set -e
 #set -x
 
-cget() { consul kv get "${1}"; }
-cput() { consul kv put "${1}" "${2}"; }
-
 # shellcheck disable=SC1091
 source /etc/profile.d/vault.sh
 
-if [ ! "$(cget vault-root-token)" ]; then # no root token in consul kv so init vault
+if [[ -f ~/.vault-root-token ]]; then
+  echo "--> Unsealing vault"
+  vault operator unseal "$(cat ~/.vault-unseal-key)"
+else
   echo "--> Initialising vault"
-  vault operator init | tee /tmp/vault.init
-  # store master keys in consul for operator to retrieve and remove
-  COUNTER=1
-  grep '^Unseal' /tmp/vault.init | awk '{print $4}' | for KEY in $(cat -); do
-    cput vault-unseal-key-${COUNTER} "${KEY}"
-    COUNTER="$((COUNTER + 1))"
-  done
-  # export root token and store in consul kv
-  ROOT_TOKEN="$(grep '^Initial' /tmp/vault.init | awk '{print $4}')" && 
-  cput vault-root-token "${ROOT_TOKEN}"
-  # shred the output
+  vault operator init -key-shares=1 -key-threshold=1 | tee /tmp/vault.init
+  KEY="$(grep '^Unseal' /tmp/vault.init | awk '{print $4}')" && \
+    echo "${KEY}" > ~/.vault-unseal-key
+  ROOT_TOKEN="$(grep '^Initial' /tmp/vault.init | awk '{print $4}')" && \
+    echo "${ROOT_TOKEN}" > ~/.vault-root-token
   shred /tmp/vault.init
+  echo "--> Unsealing vault"
+  vault operator unseal "$(cat ~/.vault-unseal-key)"
 fi
-
-# unseal vault
-echo "--> Unsealing vault"
-vault operator unseal "$(cget vault-unseal-key-1)"
-vault operator unseal "$(cget vault-unseal-key-2)"
-vault operator unseal "$(cget vault-unseal-key-3)"
