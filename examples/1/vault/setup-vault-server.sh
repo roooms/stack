9b7ca18fa572a6ebf7a074b19fe288f44a60f640
Dan Brown <roooms@gmail.com>
2018-12-02 13:26:19 +0000

    Use vault enterprise on single server

--- examples/1/vault/setup-vault-server.sh
+++ examples/1/vault/setup-vault-server.sh
@@ -2,12 +2,12 @@
 set -e
 #set -x
 
-BOOTSTRAP_EXPECT="1"
-RETRY_JOIN="11"
+BOOTSTRAP_EXPECT="${1:-"1"}"
+RETRY_JOIN="${2:-"11"}"
 
 /vagrant/bin/configure-dnsmasq.sh
 /vagrant/bin/install-app.sh consul
-/vagrant/bin/install-app.sh vault
+/vagrant/bin/install-app.sh vault 0.11.5 vault-enterprise_0.11.5+prem_linux_amd64.zip
 
 /vagrant/bin/configure-consul-server.sh ${BOOTSTRAP_EXPECT} ${RETRY_JOIN}
 /vagrant/bin/configure-vault-server.sh
